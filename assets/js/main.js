f8220c0dfae06f97eb69f9b94462e379ebb57f64
Javier Cables <101280619+ysyDoge@users.noreply.github.com>
2023-01-26 18:50:05 +0100

    Dark/Light Mode Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,19 +88,19 @@ const lightTheme = 'light-theme'
 const iconTheme = 'bx-sun'
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+//const selectedTheme = localStorage.getItem('selected-theme')
+//const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the light-theme class
 const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
 
 // We validate if the user previously chose a topic
-if (selectedTheme) {
+/*if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
   themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
-}
+}*/
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
